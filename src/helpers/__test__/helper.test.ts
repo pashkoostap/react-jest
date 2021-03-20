f93030d72fc65ee932546e30142e259525f287ba
Pashko Ostap <pashko.ostap@gmail.com>
2021-03-20 14:01:19 +0200

    fixed tests

--- src/helpers/__test__/helper.test.ts
+++ src/helpers/__test__/helper.test.ts
@@ -2,6 +2,6 @@ import myHelper from '../helper';
 
 describe('#myHelper', () => {
   it('should return 5', () => {
-    expect(myHelper({ a: 2, b: 3 })).toEqual(4);
+    expect(myHelper({ a: 2, b: 3 })).toEqual(5);
   });
 });
